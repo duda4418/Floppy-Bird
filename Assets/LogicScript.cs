befd4d01d7aa69f13633d37cfe65cf37681fdfd9
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-08-30 23:24:00 +0300

    Game over and Try again

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
     public Text scoreText;
     public int playerScore;
+    public GameObject GameOverScreen;
 
     [ContextMenu("Increase score")]
     public void addScore(int points)
@@ -15,4 +17,13 @@ public class LogicScript : MonoBehaviour
         scoreText.text = playerScore.ToString();
     }
 
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        GameOverScreen.SetActive(true);
+    }
 }
